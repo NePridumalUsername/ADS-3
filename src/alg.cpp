d38fb8233d38ae1b2c03c2567770d7f9fc396e1b
Ivanov Vladimir <99176333+NePridumalUsername@users.noreply.github.com>
2022-03-13 12:00:09 +0300

    hihiHAHA.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+   int left = 0;
+    int right = size;
+    int midd = 0;
+    while (1)
+    {
+        midd = (left + right) / 2;
+        if (value < arr[midd])
+            right = midd - 1;
+        else if (value > arr[midd])
+            left = midd + 1;
+        else
+            return midd;
+        if (left > right)
+            return 0;
+    }
 }
