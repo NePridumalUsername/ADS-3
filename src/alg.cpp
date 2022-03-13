0c49c538176f23b6c7126f33f1799a1aa06c1abe
Ivanov Vladimir <99176333+NePridumalUsername@users.noreply.github.com>
2022-03-13 15:58:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,27 +1,20 @@
+// Copyright 2021 NNTU-CS
+
 int cbinsearch(int* arr, int size, int value) {
+int count = 0;
 int left = 0;
 int right = size;
-int midd = 0;
-static int count = 0;
-while (1) {
-midd = (left + right) / 2;
-if (value < arr[midd]) {
-right = midd - 1;
-} else if (value > arr[midd]) {
-left = midd + 1;
+while (left < right) {
+int mid = (left + right) / 2;
+if (arr[mid] < value) {
+left = mid + 1;
 } else {
+right = mid;
+}
+}
+while (arr[left] == value) {
 count++;
-arr[midd] = -1;
-for (int i = midd -1; i >= 0; i--, midd--) {
-int temp = arr[i];
-arr[i] = arr[midd];
-arr[midd] = temp;
-}
-cbinsearch(arr, size, value);
-break;
-}
-if (left > right)
-break;
+left++;
 }
 return count;
 }
